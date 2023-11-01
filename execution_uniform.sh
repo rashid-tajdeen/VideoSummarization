1b8160362f098190f82b6dd25731e1b460098251
Rashid Tajdeen <rashid.tajdeen@icy3d.com>
2023-11-01 21:36:07 +0100

    update execution script

--- execution_uniform.sh
+++ execution_uniform.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+
 #SBATCH --job-name=dia-ret-data
 #SBATCH --partition=gpu #set to GPU for GPU usage
 #SBATCH --nodes=1              # number of nodes
-#SBATCH --mem=600MB               # memory per node in MB (different units with$
+#SBATCH --mem=180GB               # memory per node in MB (different units with$
 #SBATCH --ntasks-per-node=64    # number of cores
 #SBATCH --time=72:00:00           # HH-MM-SS
-#SBATCH --output /home/nmathi2s/perl5/job_tf.%N.%j.out # filename for STDOUT (%N: nodename, %j: j$
-#SBATCH --error /home/nmathi2s/perl5/job_tf.%N.%j.err  # filename for STDERR
+#SBATCH --output /home/rtajde2s/perl5/job_tf.%N.%j.out # filename for STDOUT (%N: nodename, %j: j$
+#SBATCH --error /home/rtajde2s/perl5/job_tf.%N.%j.err  # filename for STDERR
 
+module load cuda
 
 # locate to your root directory
 cd /home/rtajde2s/VideoSummarization/src
 
 # run the script
-python3 main.py --frame_selection uniform --train --valid
+python3 main.py --frame_selection uniform --epochs 50 --classes 17 --train --valid
 
 #Find your batch status in https://wr0.wr.inf.h-brs.de/wr/stat/batch.xhtml
 #To start training run "sbatch execution.sh"
