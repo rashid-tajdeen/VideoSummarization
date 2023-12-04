0be84312c296b1bd351387900e531fd0df28476e
Rashid Tajdeen <rashid.tajdeen@icy3d.com>
2023-12-04 13:13:31 +0100

    Modify execution script

--- execution_uniform.sh
+++ execution_uniform.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=180GB               # memory per node in MB (different units with$
 #SBATCH --ntasks-per-node=64    # number of cores
 #SBATCH --time=72:00:00           # HH-MM-SS
-#SBATCH --output /home/rtajde2s/perl5/job_tf.%N.%j.out # filename for STDOUT (%N: nodename, %j: j$
-#SBATCH --error /home/rtajde2s/perl5/job_tf.%N.%j.err  # filename for STDERR
+#SBATCH --output execution_logs/job_tf.%N.%j.out # filename for STDOUT (%N: nodename, %j: j$
+#SBATCH --error execution_logs/job_tf.%N.%j.err  # filename for STDERR
 
 module load cuda
 
@@ -15,7 +15,7 @@ module load cuda
 cd /home/rtajde2s/VideoSummarization/src
 
 # run the script
-python3 main.py --frame_selection uniform --epochs 50 --classes 17 --train --valid
+python3 main.py --frame_selection uniform --epochs 50 --classes 17 --train --valid --early_stopping
 
 #Find your batch status in https://wr0.wr.inf.h-brs.de/wr/stat/batch.xhtml
 #To start training run "sbatch execution.sh"
